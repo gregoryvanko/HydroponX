2e9ef6d856460216d1dbed0f06805506ea7701c8
gregoryvanko <gregory.vankoninckxloo@gmail.com>
2026-05-31 23:59:13 +0200

    Add files via upload

--- SRC/Module-Gateway/Config.h
+++ SRC/Module-Gateway/Config.h
@@ -1,4 +1,24 @@
 #pragma once
 
+// ── Loop ──────────────────────────────────────────────────────────────────────
 constexpr uint16_t CONFIG_BAUDRATE = 9600;
 constexpr uint32_t CONFIG_MESURE_INTERVAL = 5000;
+
+// ── WiFi ──────────────────────────────────────────────────────────────────────
+constexpr char* CONFIG_WIFI_SSID     = "blacknet-IOT";
+constexpr char* CONFIG_WIFI_PASSWORD = "gregoryvk99iot";
+
+// ── MQTT ──────────────────────────────────────────────────────────────────────
+static const char* CONFIG_MQTT_BROKER   = "192.168.40.40";  // IP ou hostname du broker
+static const uint16_t CONFIG_MQTT_PORT  = 1883;
+static const char* CONFIG_MQTT_CLIENT   = "ESP32-Hydro1";
+static const char* CONFIG_MQTT_USER     = "gregory";     // nullptr si pas d'auth
+static const char* CONFIG_MQTT_PASS     = "gregory";
+ 
+// ── Topics ────────────────────────────────────────────────────────────────────
+static const char* CONFIG_TOPIC_waterTemp    = "hydro1/waterTemp";
+static const char* CONFIG_TOPIC_waterEc      = "hydro1/waterEc";
+static const char* CONFIG_TOPIC_waterLevel   = "hydro1/waterLevel";
+static const char* CONFIG_TOPIC_waterPresent = "hydro1/waterPresent";
+static const char* CONFIG_TOPIC_CMD          = "hydro1/cmd/#";  // wildcard
+static const char* CONFIG_TOPIC_STATUS       = "hydro1/status";
--- SRC/Module-Gateway/Module-Gateway.ino
+++ SRC/Module-Gateway/Module-Gateway.ino
@@ -2,18 +2,36 @@
 
 #include <Arduino.h>
 #include "Config.h"
+#include "SensorData.h"
+#include "espnow_comm.h"
+#include <WiFi.h>
+#include "WifiHelper.h"
+#include "MqttGateway.h"
 
-// Structure de l'object contenant les mesures des capteurs
-typedef struct {
-  float  waterTemp;       // °C
-  float  waterEc;         // mS/cm
-  float  waterLevel;      // cm
-  bool   waterPresent;    // true = flux détecté
-  uint32_t timestamp;     // millis() au moment de l'envoi
-} SensorData_t;
-
-// Deniere mesure (ms)
+// ── Objets globaux ────────────────────────────────────────────────────────────
+WiFiClient  wifiClient;
+MqttGateway mqtt(wifiClient, CONFIG_MQTT_BROKER, CONFIG_MQTT_PORT, CONFIG_MQTT_CLIENT);
 uint32_t lastMesure = 0;
+
+// ─────────────────────────────────────────────────────────────────────────────
+// Callback réception MQTT
+// ─────────────────────────────────────────────────────────────────────────────
+void onMqttMessage(const String& topic, const String& payload) {
+    Serial.printf("[APP] Message reçu — topic: %s | payload: %s\n",
+                  topic.c_str(), payload.c_str());
+ 
+    // Exemple de dispatch par topic
+    if (topic == "hydro/cmd/pump") {
+      bool on = (payload == "ON" || payload == "1");
+      Serial.printf("[APP] Commande pompe : %s\n", on ? "MARCHE" : "ARRÊT");
+      // digitalWrite(PIN_PUMP, on ? HIGH : LOW);
+    }
+    else if (topic == "hydro/cmd/light") {
+      // …
+    } else {
+      Serial.printf("[APP] topic not found");
+    }
+}
 
 // ---setup------------------------------------------------
 void setup() {
@@ -24,6 +42,26 @@ void setup() {
   // Setup start
   Serial.println("\nsetup: start");
 
+  // Wifi connection to routeur
+  connectToWiFi(CONFIG_WIFI_SSID, CONFIG_WIFI_PASSWORD);
+
+  // MQTT connection
+  mqtt.setCredentials(CONFIG_MQTT_USER, CONFIG_MQTT_PASS);
+  mqtt.setLastWill(CONFIG_TOPIC_STATUS, "offline", /*retain=*/true, /*qos=*/1);
+  mqtt.setMessageCallback(onMqttMessage);
+  if (!mqtt.connect(5)) {
+      Serial.println("[APP] Impossible de se connecter au broker. Redémarrage…");
+      ESP.restart();
+  }
+  mqtt.subscribe(CONFIG_TOPIC_CMD, 1);
+  mqtt.publish(CONFIG_TOPIC_STATUS, "online", /*retain=*/true);
+
+  // ESP-NOW (Pas de pair à enregistrer côté récepteur → nullptr)
+  if (!espnowComm.begin(nullptr,true)) {
+      Serial.println("FATAL : Initialisation ESP-NOW échouée. Redémarrage...");
+      delay(3000);
+      ESP.restart();
+  }
 
   // Setup done
   Serial.println("setup: done");
@@ -31,10 +69,45 @@ void setup() {
 
 // ---loop-------------------------------------------------
 void loop() {
+  // Reconnexion Wi-Fi si nécessaire (non bloquant)
+  handleWiFiReconnect();
+
+  // Maintien connexion mqtt + réception messages
+  mqtt.loop();
+
   // Initialisation de la mesure du temps
   uint32_t now = millis();
   // Si l'intervale de temps s'est écoulé
   if (now - lastMesure >= CONFIG_MESURE_INTERVAL){
+    // Actualisation de la mesure du temps
+    lastMesure = now;
   }
-  
+
+  // Réception message ESP-NOW
+  if (espnowComm.hasNewMessage()) {
+      // Get last message
+      String   msg = espnowComm.getLastMessage();
+      // Désérialisation → SensorData_t
+      SensorData_t parsed;
+      bool deserialisationIsOk = stringToSensorData(msg, parsed);
+      // si la deserialisation est OK
+      if (deserialisationIsOk) {
+          // Print mesures
+          Serial.printf("[DATA] Temp=%.2f°C  EC=%.2fmS/cm  Niveau=%.1fcm  Flux=%s\n",
+              parsed.waterTemp,
+              parsed.waterEc,
+              parsed.waterLevel,
+              parsed.waterPresent ? "OUI" : "NON");
+      } else {
+          Serial.println("ERREUR : parsing SensorData_t échoué !");
+      }
+
+      // send MQTT
+      mqtt.publish(CONFIG_TOPIC_waterTemp, String(parsed.waterTemp, 2));
+      mqtt.publish(CONFIG_TOPIC_waterEc, String(parsed.waterEc, 2));
+      mqtt.publish(CONFIG_TOPIC_waterLevel, String(parsed.waterLevel, 2));
+      mqtt.publish(CONFIG_TOPIC_waterPresent, String(parsed.waterPresent ? "1" : "0"));
+
+      Serial.println("========================");
+  }
 }
